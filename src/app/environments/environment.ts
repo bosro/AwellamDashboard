24b08e46c8cb60c1c02717307880a12651b3d442
DixonAfreh <dixonafreh20@gmail.com>
2025-01-22 22:18:17 +0000

    new

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    apiUrl: 'https://newawellambcckend.onrender.com/api'
+    // apiUrl: 'https://newawellambcckend.onrender.com/api'
+    apiUrl:"http://0.0.0.0:3000/api"
 }
 
 export const url ={
