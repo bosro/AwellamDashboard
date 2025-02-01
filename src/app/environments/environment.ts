33a08f3888c5ad8aec403a987229aba10ce103e4
DixonAfreh <dixonafreh20@gmail.com>
2025-02-01 22:57:43 +0000

    new updates fuxed

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // apiUrl: 'https://newawellambcckend.onrender.com/api'
-    // apiUrl:"http://127.0.0.1:3000/api"
+    apiUrl:"http://127.0.0.1:3000/api"
     // apiUrl:"https://awellamgh.com/api"
 }
 
